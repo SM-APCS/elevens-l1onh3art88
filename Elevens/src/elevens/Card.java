7b5fc7041d3ed524604cf74240792f1981cb8712
l1onh3art88 <spenccheng@gmail.com>
2016-01-15 07:23:05 -0600

    Card Elevens

---
+++ Elevens/src/elevens/Card.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package elevens;
+
+/**
+ *
+ * @author spenc_000
+ */
+public class Card {
+    
+	private String suit;
+	private String rank;
+	private int pointValue;
+
+
+	public Card(String cardRank, String cardSuit, int cardPointValue) {
+		//initializes a new Card with the given rank, suit, and point value
+		rank = cardRank;
+		suit = cardSuit;
+		pointValue = cardPointValue;
+	}
+
+	public String suit() {
+		return suit;
+	}
+
+	public String rank() {
+		return rank;
+	}
+
+	public int pointValue() {
+		return pointValue;
+	}
+
+        //Compares card value to another card
+	public boolean matches(Card otherCard) {
+		return otherCard.suit().equals(this.suit())
+			&& otherCard.rank().equals(this.rank())
+			&& otherCard.pointValue() == this.pointValue();
+	}
+
+	//Creates a string version of the Card class. 
+	@Override
+	public String toString() {
+		return rank + " of " + suit + " (point value = " + pointValue + ")";
+	}
+}
